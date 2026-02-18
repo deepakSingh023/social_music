f8981db39d9f61ada5b9a63aa54d94a2cdbb156a
deepak singh <157205606+deepakSingh023@users.noreply.github.com>
2026-02-19 02:24:10 +0530

    Add RequestMapping to HealthController

--- src/main/java/com/example/social_music/controllers/HealthController.java
+++ src/main/java/com/example/social_music/controllers/HealthController.java
@@ -1,6 +1,8 @@
 package com.example.social_music.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RequestMapping("/api")
