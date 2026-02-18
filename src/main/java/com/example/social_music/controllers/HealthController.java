5ecf4c73d8de726bf06a5752451c7fef85e67f49
deepak singh <157205606+deepakSingh023@users.noreply.github.com>
2026-02-19 02:21:18 +0530

    Change health endpoint to '/health'

--- src/main/java/com/example/social_music/controllers/HealthController.java
+++ src/main/java/com/example/social_music/controllers/HealthController.java
@@ -2,10 +2,12 @@ package com.example.social_music.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping("/api")
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String root() {
         return "Social Music Service Running";
     }
